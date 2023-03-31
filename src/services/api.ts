43be478c4e25cc535b0fd8f2c97e48607a24a6fd
shaikh-kamran <shaikhkamran959@gmail.com>
2023-03-31 14:31:06 +0530

    dist added

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseURL = `https://${window.location.hostname}:${window.location.port}/api`;
+const baseURL = `http://${window.location.hostname}:${window.location.port}/api`;
 // const baseURL = "http://localhost:5000/api";
 
 export default {
